61b6fc32d6c9879244e4a0a1075789dfc8e68d78
fanisabrilliant <fanisaprastyo@gmail.com>
2026-04-05 17:09:59 +0700

    Penambahan fitur merubah Kilogram ke Pons atau Gram

--- main.c
+++ main.c
@@ -6,6 +6,16 @@ void konversiJarak();  // Anggota 2
 void konversiBerat();  // Anggota 3
 void konversiWaktu();  // Anggota 4
 
+
+// ANGGOTA 3: KONVERSI BERAT
+// ==========================================
+void konversiBerat() {
+    float kg;
+    printf("\n--- Konversi Berat ---\n");
+    printf("Masukkan berat dalam Kilogram: ");
+    scanf("%f", &kg);
+    printf("Gram: %.2f g\n", kg * 1000);
+    printf("Pons (Lbs): %.2f lbs\n", kg * 2.20462);
 // ==========================================
 // KETUA KELOMPOK & ANGGOTA 5
 // Bagian: Menu Utama & Navigasi
